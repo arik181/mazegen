84c66f3184e4d09b601b1332dae4e674a1d7384e
arik181 <arik181@hotmail.com>
2009-04-02 03:33:02 -0700

    Added README, qtree.*.

---
+++ qtree.h
@@ -0,0 +1,23 @@
+#include <cstring>
+#include <ncurses.h>
+
+using namespace std;
+
+#ifndef QTREE
+#define QTREE
+
+class qtree
+{
+	public:
+
+		/*** Default Constructor ***/
+		qtree();
+
+		/*** Destructor ***/
+		~qtree();
+
+	private:
+
+}
+
+#endif // QTREE
