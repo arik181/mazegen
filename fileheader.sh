50c254f031c3def7949ec374aba2f219d5b39375
Arik Oneeightone <arik181@BeastIV.(none)>
2009-04-19 22:03:43 -0700

    Fixed a bug in the fileheader creation script.

--- fileheader.sh
+++ fileheader.sh
@@ -10,15 +10,17 @@ for x in ./$1/*.h; do vi ${x}; done
 for x in *.h; do echo "* " >> ./$1/${x}; done
 for x in *.h; do echo "* ***/" >> ./$1/${x}; done
 for x in *.h; do echo "" >> ./$1/${x}; done
+for x in *.h; do cat ${x} >> ./$1/${x}; done
 
 
 for x in *.cpp; do echo "/*** ${x}" >> ./$1/${x}; done
 for x in *.cpp; do cat header.txt.tmp ./$1/${x} >> ./$1/${x}; done
-for x in *.cpp; do echo "* Purpose: ***/" >> ./$1/${x}; done
+for x in *.cpp; do echo "* Purpose:" >> ./$1/${x}; done
 for x in *.cpp; do echo "* " >> ./$1/${x}; done
 for x in ./$1/*.cpp; do vi ${x}; done
 for x in *.cpp; do echo "* " >> ./$1/${x}; done
 for x in *.cpp; do echo "* ***/" >> ./$1/${x}; done
 for x in *.cpp; do echo "" >> ./$1/${x}; done
+for x in *.cpp; do cat ${x} >> ./$1/${x}; done
 
 rm -rf header.txt.tmp
